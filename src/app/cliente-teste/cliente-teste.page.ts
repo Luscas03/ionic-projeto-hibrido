3710f60130217842bb2e221dd3fe7b1dd2b7709e
daniel <daniel.cavalcanti@outlook.com.br>
2021-02-19 09:57:09 -0300

    Página Cliente teste

---
+++ src/app/cliente-teste/cliente-teste.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-teste',
+  templateUrl: './cliente-teste.page.html',
+  styleUrls: ['./cliente-teste.page.scss'],
+})
+export class ClienteTestePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
