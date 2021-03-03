04c2f732c9fd1784be53221492acb2c7cb8e4224
Alex Alves <58983762+alexalvesz@users.noreply.github.com>
2021-03-03 10:27:09 -0300

    att

--- src/app/marcar-consulta/marcar-consulta.page.ts
+++ src/app/marcar-consulta/marcar-consulta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { FormBuilder, FormGroup} from '@angular/forms';
 import { NavController } from '@ionic/angular';
 import { ConsultaService } from '../service/consulta.service';
@@ -12,25 +13,38 @@ import { TemplateService } from '../service/template.service';
 })
 export class MarcarConsultaPage implements OnInit {
   formGroup: FormGroup;
+  idcliente : string = "";
 
 
   constructor(private formB: FormBuilder,
     private navCtrl: NavController,
     private template: TemplateService,
-    private consultaServ: ConsultaService) {
+    private consultaServ: ConsultaService,
+    private auth : AngularFireAuth) {
     this.iniciarForm();
+
+    this.auth.authState.subscribe(response=>{ // dados usuário logado
+      this.idcliente = response.uid; // id do usuário logado
+      this.iniciarForm(); // atualizando formulário com o id do cliente
+    })
   }
   
   ngOnInit() {
   }
 
   cadastrar() {
+ 
+    
+    let d = new Date(this.formGroup.controls['date'].value);
+    console.log(d.getDay());
+    
+
     this.template.loading.then(load => { // iniciar o carregamento
       load.present(); // forçar inicio carremento
-
       this.consultaServ.cadastrar(this.formGroup.value).subscribe(response => {
         load.dismiss();
         this.template.myAlert(response);
+        
       })
 
     })
@@ -40,8 +54,7 @@ export class MarcarConsultaPage implements OnInit {
       tipo: [],
       especialidade: [],
       date: [],
-      hora: []
-
+      idcliente: [this.idcliente]
     })
   }
 
