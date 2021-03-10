c097defae2aec86d7e3b94a87d83be4d10592ee0
daniel <daniel.cavalcanti@outlook.com.br>
2021-03-10 09:48:14 -0300

    Upload Imagem Perfil

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { NavController } from '@ionic/angular';
 import { Cliente } from '../model/cliente';
 import { ClienteService } from '../service/cliente.service';
@@ -13,11 +14,14 @@ export class PerfilPage implements OnInit {
 
   uid : string = null;
   cliente : Cliente = new Cliente();
+  imagem : any; // imagem do perfil enviado/baixado/url
 
   constructor(
     private clienteServ : ClienteService,
     private auth: AngularFireAuth,
-    private navCtrl: NavController) {
+    private navCtrl: NavController,
+    private fireStorage : AngularFireStorage// biblioteca para o storage firebase
+    ) {
 
       this.auth.authState.subscribe(response=>{
         this.uid = response.uid;
@@ -33,6 +37,7 @@ export class PerfilPage implements OnInit {
             // perfil existe
             this.clienteServ.obterPerfil(this.uid).subscribe(data=>{
               this.cliente = data;
+              this.downloadImage(); // Realiza o donwload da imagem no início
             })
           }
 
@@ -47,6 +52,17 @@ export class PerfilPage implements OnInit {
 
 atualizar(){
   this.navCtrl.navigateRoot('/perfil-atualizar');
+}
+
+
+downloadImage(){
+  // faz o donwload da imagem na pasta perfil
+  // nome da imagem é o id do usuario + jpg
+  this.fireStorage.storage.ref().child(`perfil/${this.uid}.jpg`).getDownloadURL().then(url=>{
+    this.imagem = url;
+  },err =>{
+    this.imagem = "https://www.cob.org.br/public/painel/img/perfil.jpg"; // imagem não existe
+  })
 }
 
 }
